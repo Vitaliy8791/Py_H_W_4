bed4181203f938eae8e5602a3f9d75687f6b8617
Vitaliy8791 <45199670+Vitaliy8791@users.noreply.github.com>
2018-12-17 21:24:05 +0200

    Update hw_4_1.py

--- hw_4_1.py
+++ hw_4_1.py
@@ -1,36 +1,50 @@
-from string import punctuation, whitespace
+from string import whitespace, punctuation, ascii_letters, digits
 
-fltterstr = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-# создаём строку из специальных, английских символов и символов пунктуации
-fltterstr1 = fltterstr + punctuation + whitespace
-
-try:
-    def clear_word(enter_string, filt):
-        enter_num = []
-        enter_char = []
-        enter_list = list(enter_string)  # преобразовываем текст в список символов текста
-        for i in range(0, len(enter_list)):
-            if enter_list[i] in filt:  # если символ в строке из специальных, английских символов и символов пунктуации
-                continue               # пропускаем его
-            else:
-                enter_char.append(enter_list[i])  # добавляем в список отобранных символов
-                enter_num.append(i)  # запоменаем его порядковый номер
-        rez_list = [enter_char, enter_num]  # создаём список кортежей из символов и порядковых номеров
-        return rez_list
+enter_num = []
+enter_char = []
+fltterstr = whitespace + punctuation + digits
 
 
-    if __name__ == "__main__":
-        text = """clear_word - Функция по очистке слова от символов не из - filterstr"""
-        enter_tupl = clear_word(text, fltterstr1)  # вызываем функцию clear_word
-        char, num = enter_tupl  # разбиваем список кортежей на список символов и список порядковых номеров
-        if char[0] is not None:  # если был хоть искомый символ вызываем ошибку
+def clear_word(enter_string, filt):  # функция проверяет текст на символы не из английского алфавита
+    enter_char = []
+    enter_list = enter_string  # преобразовываем текст в список символов текста
+    for i in range(0, len(enter_list)):
+        if enter_list[i] in filt:  # если символ в строке из специальных символов и символов пунктуации пропускаем его
+            continue
+        if enter_list[i] not in ascii_letters:  # если символ в строке не из английского алфавита вызываем исключение
             raise ValueError
+        enter_char.append(enter_list[i])
 
-except IndexError:  # ошибка если небыло нужных символов
+    return enter_char
+
+
+def bad_char(text):  # функция проверяет текст на символы не из английского алфавита
+    enter_list = list(text)
+    for i in range(0, len(enter_list)):
+        if enter_list[i] in fltterstr:  # пропускаем специальные символы и символы пунктуации
+            continue
+        if enter_list[i] not in ascii_letters:
+            enter_char.append(enter_list[i])  # добавляем в список отобранных символов
+            enter_num.append(i)  # запоменаем его порядковый номер
+    return enter_char, enter_num
+
+
+if __name__ == "__main__":
+    text = """clear_word - Функция по очистке слова от символов не из - filterstr"""
+try:
+    enter_list = clear_word(text, fltterstr)  # вызываем функцию clear_word
     print("В тексте только специальные, английские символы и символы пунктуации!")
+
+
+
+
 except ValueError:  # выводим результат поиска
+    print("В тексте найдены символы не из английскго алфавита!\n")
+    bad_list, bad_num = bad_char(text)
+    print(bad_list)
+    print(bad_num)
     print("В тексте найдены символы не из английскго алфавита!\n")
     print("| {:^3}  | {:^5} |".format("СИМВОЛ", "НОМЕР ПОЗИЦИИ"))
     print("|_________|_______________|")
-    for i in range(0, len(char)):
-        print("| {:6}  | {:>13} |".format(char[i], num[i]))
+    for i in range(0, len(bad_list)):
+        print("| {:6}  | {:>13} |".format(bad_list[i], bad_num[i]))
